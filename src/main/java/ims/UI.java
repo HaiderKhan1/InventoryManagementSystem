380499030ed0d88693b0946f1b02e56b17aa7646
Haider Khan <hkhan24@uoguelph.ca>
2020-12-15 14:02:27 -0500

    Added new files to work on
    Read the UI java doc to see what the application layout. Parser class added from Haider's branch.

--- src/main/java/ims/UI.java
+++ src/main/java/ims/UI.java
@@ -2,6 +2,20 @@ package ims;
 
 public class UI {
 
+/**
+This class will act as the "main class for the application"
+
+**** On Inital StartUp *****
+
+  - A new terminal Main screen (North south east west layout)
+  - Top centre " Inventroy Management System" North centre JTEXTfeild
+  - Middle: Button One -> First Button ( Costumer View ) -> instnentiate a costumer object
+  - South middle: button two -> Second button ( Management View ) -> instentiate the manager view
+
+
+*/
+
+
   public UI() {
 
   }
